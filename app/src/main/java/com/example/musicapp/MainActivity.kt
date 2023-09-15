0d428a8bee2cfb23a276298578fc185e40cca934
AnderLH <ander.lopezdevallejohi@elorrieta-errekamari.com>
2023-09-15 21:03:55 +0200

    constraint refactor

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.musicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonPlay: ImageButton = findViewById<ImageButton>(R.id.imageButtonPlay)
+        val buttonPause: ImageButton = findViewById<ImageButton>(R.id.imageButtonPause)
+
+
+        buttonPlay.setOnClickListener {
+            buttonPlay.visibility = View.INVISIBLE
+            buttonPause.visibility = View.VISIBLE
+        }
+
+        buttonPause.setOnClickListener {
+            buttonPause.visibility = View.INVISIBLE
+            buttonPlay.visibility = View.VISIBLE
+        }
+
     }
 }
