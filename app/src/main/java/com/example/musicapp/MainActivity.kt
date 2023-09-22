c186cd02597ce32e7e286f501147cb2c21a2b215
AnderLH <ander.lopezdevallejohi@elorrieta-errekamari.com>
2023-09-22 20:42:53 +0200

    save intace

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -5,25 +5,67 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private var bottonpressed: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonPlay: ImageButton = findViewById<ImageButton>(R.id.imageButtonPlay)
-        val buttonPause: ImageButton = findViewById<ImageButton>(R.id.imageButtonPause)
+        val play: ImageButton = findViewById<ImageButton>(R.id.imageButtonPlay)
+        val pause: ImageButton = findViewById<ImageButton>(R.id.imageButtonPause)
 
-
-        buttonPlay.setOnClickListener {
-            buttonPlay.visibility = View.INVISIBLE
-            buttonPause.visibility = View.VISIBLE
+        play.setOnClickListener {
+            play.visibility = View.INVISIBLE
+            pause.visibility = View.VISIBLE
+            bottonpressed = true
         }
 
-        buttonPause.setOnClickListener {
-            buttonPause.visibility = View.INVISIBLE
-            buttonPlay.visibility = View.VISIBLE
+        pause.setOnClickListener {
+            pause.visibility = View.INVISIBLE
+            play.visibility = View.VISIBLE
+            bottonpressed = false
         }
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val play : ImageView = findViewById<ImageView>(R.id.imageButtonPlay)
+        val pause : ImageView = findViewById<ImageView>(R.id.imageButtonPause)
+        if (bottonpressed) {
+            pause.visibility = View.VISIBLE
+            play.visibility = View.INVISIBLE
+        }else {
+            pause.visibility = View.INVISIBLE
+            play.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        // Save the user's current game state.
+        outState?.run {
+            putBoolean("result", bottonpressed)
+        }
+        // Always call the superclass so it can save the view hierarchy.
+        super.onSaveInstanceState(outState)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        // Always call the superclass so it can restore the view hierarchy.
+        super.onRestoreInstanceState(savedInstanceState)
+        // Restore state members from saved instance.
+        savedInstanceState?.run {
+            bottonpressed = savedInstanceState.getBoolean("result")
+            val play : ImageView = findViewById<ImageView>(R.id.imageButtonPlay)
+            val pause : ImageView = findViewById<ImageView>(R.id.imageButtonPause)
+            if(bottonpressed) {
+                pause.visibility = View.VISIBLE
+                play.visibility = View.INVISIBLE
+            }else {
+                pause.visibility = View.INVISIBLE
+                play.visibility = View.VISIBLE
+            }
+        }
     }
 }
